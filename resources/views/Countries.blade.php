c6efe1429bd86176a3c9b2a706fa3c626fd0d44c
blizzend <xcrobertasxc@gmail.com>
2022-09-23 11:43:44 +0300

    beleka

---
+++ resources/views/Countries.blade.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">South Airlines</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Airports</a>
+        <a class="nav-link" href="/Countries">Countries</a>
+        <a class="nav-link" href="#">Airlines</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">Lithuania</option>
+  <option value="2">America</option>
+  <option value="3">Germany</option>
+  <option value="3">Poland</option>
+  <option value="3">South Africa</option>
+</select>
+  
+<table class="table table-dark">
+  <thead>
+  
+  <tbody>
+    <tr class="table-active">
+      
+    </tr>
+    <tr>
+     
+    </tr>
+    <tr>
+      <th scope="row">Name</th>
+      <th scope="row">Country</th>
+      <th scope="row">Location</th>
+      <th scope="row">Airlines</th>
+      <th scope="row">Actions</th>
+      <th></th>
+      <th></th>
+      <th></th>
+      
+    </tr>
+    
+    <tr class="table-active">
+      <th scope="row">sgdsg</th>
+      <th scope="row">Lithuania</th>
+      <th scope="row">54.6817023, 25.272688</th>
+      <th scope="row">Lithuania Airlines</th>
+      <td> <a class="btn btn-success"> Add Airlines</a></td>
+      <td> <a class="btn btn-danger"> Remove Airlines</a></td><td>
+      <td> <a class="btn btn-warning">Edit</a></td>
+      </tr>
+      
+
+    <tr class="table-active">
+      <th scope="row">lkgsd</th>
+      <th scope="row">America</th>
+      <th scope="row">26.396348660003 114.656359860117</th>
+      <th scope="row">America Airlines</th>
+      <td> <a class="btn btn-success"> Add Airlines</a></td>
+      <td> <a class="btn btn-danger"> Remove Airlines</a></td>
+      <td> <a class="btn btn-warning">Edit</a></td>
+      <th></th>
+      </tr>
+
+      <tr class="table-active">
+      <th scope="row">jffjj</th>
+      <th scope="row">Germany</th>
+      <th scope="row">601672.02657396 5669093.1290685</th>
+      <th scope="row">Germany Airlines</th>
+      <td> <a class="btn btn-success"> Add Airlines</a></td>
+      <td> <a class="btn btn-danger"> Remove Airlines</a></td>
+      <td> <a class="btn btn-warning">Edit</a></td>
+      <th></th> 
+      </tr> 
+
+      <tr class="table-active">
+      <th scope="row">fjfjy</th>
+      <th scope="row">Poland</th>
+      <th scope="row">510235.15935331 5694599.5658881</th>
+      <th scope="row">Poland Airlines</th>
+      <td> <a class="btn btn-success"> Add Airlines</a></td> 
+      <td> <a class="btn btn-danger"> Remove Airlines</a></td>
+      <td> <a class="btn btn-warning">Edit</a></td>
+      <th></th> 
+      </tr>
+
+      <tr class="table-active">
+      <th scope="row">jgjdr</th>
+      <th scope="row">South Africa</th>
+      <th scope="row">272161.29997692 6847498.1578986</th>
+      <th scope="row">Africa Airlines</th>
+      <td> <a class="btn btn-success"> Add Airlines</a></td> 
+      <td> <a class="btn btn-danger"> Remove Airlines</a></td>
+      <td> <a class="btn btn-warning">Edit</a></td>
+      <th></th>  
+    </tr>
+    </thead>
+  </tbody>
+</table>
+  
+  
+  
+  
+  
+  
+  
+  </body>
+</html>
